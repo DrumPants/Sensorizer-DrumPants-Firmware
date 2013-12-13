bd6febfe63a1f9dd63e9c915418152e60b8475db
odbol <git@odbol.com>
2013-12-12 18:13:40 -0800

    Updated scales to support 9 sensors

--- SensorizerServer/SensorizerServer.h
+++ SensorizerServer/SensorizerServer.h
@@ -16,7 +16,7 @@
 #include "Debug.h"
 
 // TODO: these need to be updated for DUE (MORE NOTES!). hacking for now
-#define NOTE_PRESETS_ELEMENT_LENGTH 6
+#define NOTE_PRESETS_ELEMENT_LENGTH 9
 #if SENSOR_INPUTS_LENGTH > NOTE_PRESETS_ELEMENT_LENGTH
   #warning "TODO: update note preset arrays for more than 6 sensors"
 #endif
