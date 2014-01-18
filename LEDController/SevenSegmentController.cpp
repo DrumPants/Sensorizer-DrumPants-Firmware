2e008a2bb9ce50435918e9ac9498ee6614b8da09
odbol <git@odbol.com>
2014-01-18 01:42:37 -0800

    Added alphanumeric support in LED

--- LEDController/SevenSegmentController.cpp
+++ LEDController/SevenSegmentController.cpp
@@ -1,32 +1,33 @@
 #include "SevenSegmentController.h"
 
 #define NUM_OUTS 7
-
+#define NUM_PRINTABLE_DIGITS 17
 
 
 // Define the LED digit patters, from 0 - 9
 // Note that these patterns are for common cathode displays
 //                                    Arduino pin: 2,3,4,5,6,7,8
-const byte seven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
-                                 { 0,1,1,0,0,0,0 },  // = 1
-                                 { 1,1,0,1,1,0,1 },  // = 2
-                                 { 1,1,1,1,0,0,1 },  // = 3
-                                 { 0,1,1,0,0,1,1 },  // = 4
-                                 { 1,0,1,1,0,1,1 },  // = 5
-                                 { 1,0,1,1,1,1,1 },  // = 6
-                                 { 1,1,1,0,0,0,0 },  // = 7
-                                 { 1,1,1,1,1,1,1 },  // = 8
-                                 { 1,1,1,0,0,1,1 }   // = 9
-
-                                 // // EXTENDOTRON
-                                 // { 1,0,1,1,0,1,1 },  // = A
-                                 // { 1,0,1,1,1,1,1 },  // = B
-                                 // { 1,1,1,0,0,0,0 },  // = C
-                                 // { 1,1,1,1,1,1,1 },  // = D
-                                 // { 1,1,1,0,0,0,0 },  // = E
-                                 // { 1,1,1,1,1,1,1 },  // = F
-                                 // { 1,1,1,1,1,1,1 }   // = BLANK_DIGIT
-                                 };
+const byte seven_seg_digits[NUM_PRINTABLE_DIGITS][NUM_OUTS] = { 
+                                      { 1,1,1,1,1,1,0 },  // = 0
+                                      { 0,1,1,0,0,0,0 },  // = 1
+                                      { 1,1,0,1,1,0,1 },  // = 2
+                                      { 1,1,1,1,0,0,1 },  // = 3
+                                      { 0,1,1,0,0,1,1 },  // = 4
+                                      { 1,0,1,1,0,1,1 },  // = 5
+                                      { 1,0,1,1,1,1,1 },  // = 6
+                                      { 1,1,1,0,0,0,0 },  // = 7
+                                      { 1,1,1,1,1,1,1 },  // = 8
+                                      { 1,1,1,0,0,1,1 },   // = 9
+                                                                            
+                                      // // EXTENDOTRON
+                                      { 1,1,1,0,1,1,1 },  // = A
+                                      { 1,1,1,1,0,1,1 },  // = B
+                                      { 1,0,0,1,1,1,0 },  // = C
+                                      { 0,1,1,1,1,0,1 },  // = D
+                                      { 1,0,0,1,1,1,1 },  // = E
+                                      { 1,0,0,0,1,1,1 },  // = F
+                                      { 0,0,0,0,0,0,0 }   // = BLANK_DIGIT
+                                      };
              
 
 #define LED_UPDATE_INTERVAL 10
@@ -76,7 +77,16 @@ void SevenSegmentController::chooseDigit(byte digit) {
 
 
 void SevenSegmentController::showChar(unsigned char c) {
-  byte num = c - '0';
+  byte num;
+
+  if (c >= 'A') {
+    num = c - 'A' + 10; // start after normal digits in array
+  }
+  else {
+    // normal digit
+    num = c - '0';  
+  }
+  
 
 //      SerialUSB.print("show char "); 
 // SerialUSB.print(NUM_OUTS);
@@ -86,6 +96,8 @@ void SevenSegmentController::showChar(unsigned char c) {
 }
   
 void SevenSegmentController::showDigit(byte num) {
+  if (num >= NUM_PRINTABLE_DIGITS) return;
+
   const byte* segs = seven_seg_digits[num];
   
   for (int i = 0; i < NUM_OUTS; i++) {
@@ -129,6 +141,12 @@ void SevenSegmentController::print(int num) {
   if (startChar < 0) {
       charactersForDigits[0] = '0';
   }
+  // else if (startChar > 0) {
+  //   // translate hundreds into hex-ish notation.
+  //   byte tens = num / 10;
+
+  //   charactersForDigits[0] = ('A' - 10) + tens;
+  // }
   else {
     charactersForDigits[0] = str.charAt(startChar + 0);
   }
