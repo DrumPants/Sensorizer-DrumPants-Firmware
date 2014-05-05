e9050f4df938e018fd4dafd8b02a9a696aab1ded
odbol <git@odbol.com>
2014-05-05 01:18:54 -0700

    Added configurator save skeleton

--- SensorizerServer/configurator/MidiInput.cpp
+++ SensorizerServer/configurator/MidiInput.cpp
@@ -1,11 +1,15 @@
 #include "MidiInput.h"
 #include <Arduino.h>
 
+#define CHANNEL_COMMAND_SAVE 127
+
 #define BLE_MIDI_SERIAL_IN (Serial1)
 #define USB_MIDI_SERIAL_IN (SerialUSB)
 
 MidiInput::MidiInput(SensorizerServer* server) {
 	this->server = server;
+
+	this->clearDirty();
 }
 
 
@@ -49,8 +53,31 @@ void MidiInput::updateField(byte sensorIdx, byte fieldIdx, byte val) {
 		if (sensorInput != NULL) {
 			float value = (float)val / 127.0;
 
-			Configurator::setField(sensorInput, fieldIdx, value);
+			if (Configurator::setField(sensorInput, fieldIdx, value)) {
+				this->setDirty(sensorIdx, fieldIdx);
+			}
 
+		}
+	}
+	// we only save when they tell us they're done editing. don't want to burn out that eeprom!
+	else if (sensorIdx == CHANNEL_COMMAND_SAVE) {
+		this->saveDirty();
+	}
+}
+
+
+void MidiInput::clearDirty() {
+	memset(&(this->dirtySensors[0]), 0, sizeof(this->dirtySensors));
+}
+
+void MidiInput::setDirty(int sensorIdx, int fieldIdx) {
+	this->dirtySensors[sensorIdx] = 1;
+}
+
+void MidiInput::saveDirty() {
+
+	for (int i = 0; i < SENSOR_INPUTS_LENGTH; i++) {
+		if (this->dirtySensors[i] > 0) {
 			// TODO: save to EEPROM
 		}
 	}
--- SensorizerServer/configurator/MidiInput.h
+++ SensorizerServer/configurator/MidiInput.h
@@ -14,6 +14,26 @@ class MidiInput {
 
 	void checkSerial(Stream* input);
 
+
+	byte dirtySensors[SENSOR_INPUTS_LENGTH];
+
+	/**
+	 * Marks the given field for the given sensor as needing to be saved to EEPROM.
+	 * @param sensorIdx from MIDI msg
+	 * @param fieldIdx  from MIDI msg
+	 */
+	void setDirty(int sensorIdx, int fieldIdx);
+
+	/**
+	 * Saves all dirty fields to the EEPROM.
+	 */
+	void saveDirty();
+
+	/**
+	 * Resets dirty flags for all sensors.
+	 */
+	void clearDirty();
+
 public:
 	MidiInput(SensorizerServer* server);
 
