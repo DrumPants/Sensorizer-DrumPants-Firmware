d24fd16edd50ec2828940c5d89140523a9f59160
odbol <git@odbol.com>
2014-05-10 18:31:34 -0700

    Fixed debug to intern all message strings to program space

--- SensorizerServer/Debug.h
+++ SensorizerServer/Debug.h
@@ -14,11 +14,11 @@
 #endif
 
 // don't use this function! it fucks things up for some reason!
-#define DEBUG_PRINT_SETUP SerialToComputer.begin(57600);SerialToComputer.println("Debug printing STARTED");
+#define DEBUG_PRINT_SETUP SerialToComputer.begin(57600);SerialToComputer.println(F("Debug printing STARTED"));
 
-#define PRINT(text) SerialToComputer.println(text);SerialToComputer.println("-----");
-#define PRINT_NUM(text, num) SerialToComputer.print(text);SerialToComputer.println(num, DEC);SerialToComputer.println("-----");
-#define PRINT_NUMS(text, num, num2) SerialToComputer.print(text);SerialToComputer.print(num, DEC);SerialToComputer.print(", ");SerialToComputer.println(num2, DEC);SerialToComputer.println("-----");
+#define PRINT(text) SerialToComputer.println(F(text));SerialToComputer.println(F("-----"));
+#define PRINT_NUM(text, num) SerialToComputer.print(F(text));SerialToComputer.println(num, DEC);SerialToComputer.println(F("-----"));
+#define PRINT_NUMS(text, num, num2) SerialToComputer.print(F(text));SerialToComputer.print(num, DEC);SerialToComputer.print(F(", "));SerialToComputer.println(num2, DEC);SerialToComputer.println(F("-----"));
  
 #if ENABLE_DEBUG_PRINTING
 	#define DEBUG_PRINT(text) PRINT(text)
