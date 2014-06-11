1ecc7b4e4fa379588106f55ec242e17c5db1c316
odbol <git@odbol.com>
2014-06-11 16:36:13 -0700

    Fixed piezo sensitivity for plastic piezos

--- SensorizerServer/SensorizerServer.cpp
+++ SensorizerServer/SensorizerServer.cpp
@@ -135,10 +135,19 @@ void SensorizerServer::loadPreset() {
 	#define FOOT_PEDAL_CUTOFF_LOW 0.55
 
 	#if PRESET >= PRESET_PREPRODUCTION	
-		// handle sensitive-ass bare peizos
-		#define PIEZO_INRANGE_HIGH 1.0;
-		#define PIEZO_CUTOFFRANGE_LOW 0.38
 
+		#define BARE_PIEZOS 0
+
+		#if BARE_PIEZOS
+			// handle sensitive-ass bare peizos
+			#define PIEZO_INRANGE_HIGH 1.0;
+			#define PIEZO_CUTOFFRANGE_LOW 0.38
+		#else
+			// handle plastic peizos
+			#define PIEZO_INRANGE_HIGH 0.6;
+			#define PIEZO_CUTOFFRANGE_LOW 0.1
+		#endif
+	
 		#define DEFAULT_FOOT_PEDAL_RETRIGGER_THRESHOLD DEFAULT_RETRIGGER_THRESHOLD
 	#elif PRESET == PRESET_BETA_RYAN
 		// handle larger peizo type
