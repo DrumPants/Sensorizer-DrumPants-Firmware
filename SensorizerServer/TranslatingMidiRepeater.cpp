467d349d544c02fed82554ab8bb31e20c0b7dd6a
odbol <git@odbol.com>
2014-07-21 01:21:30 -0700

    Fixed sending 1-based MIDI sensor indexes. 0 based FTW

--- SensorizerServer/TranslatingMidiRepeater.cpp
+++ SensorizerServer/TranslatingMidiRepeater.cpp
@@ -14,8 +14,8 @@ void TranslatingMidiRepeater::onSendOutput(byte cmd, byte data1, byte data2) {
     int idx = server->getSensorIdxForNote(data1);
 
     if (idx != -1) {
-    	// don't send a 0 note. that's creepy
-	    MidiRepeater::onSendOutput(cmd, idx + 1, data2);
+    	// send a 0 note. sure, why not? it's creepy, but whatevs
+	    MidiRepeater::onSendOutput(cmd, idx, data2);
 	}
 	else {
 		DEBUG_PRINT("Failed to find sensor index");
