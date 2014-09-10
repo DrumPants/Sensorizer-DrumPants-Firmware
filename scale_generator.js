043234e812d10fc1323a07f45c0934e23f2a0b21
odbol <git@odbol.com>
2014-09-10 15:31:32 +0800

    Added a ton more melodic scales. Really need to organize them better somehow

--- scale_generator.js
+++ scale_generator.js
@@ -6,7 +6,9 @@ Generates scales
 //major pent
 //var n = [0, 60, 64, 65, 67, 69];
 //minor pentatonic
-var scales = {
+var // length of the final array we want.
+	TOTAL_ARRAY_LEN = 12,
+	scales = {
 		major : [0, 2, 4, 5, 7, 9, 11, 12],
 		minor : [0, 2, 3, 5, 7, 8, 10, 12],
 		melodicMinor : [0, 2, 3, 5, 7, 9, 11, 12],
@@ -14,30 +16,61 @@ var scales = {
 		persian : [0, 1, 4, 5, 7, 8, 10, 12]
 	},
 	roots = [60 - 12 * 2, 60, 60 + 12 * 2],
-	ccode = "",
-	printCode = function (str) {
+	results = {
+		ccode: "",
+		ccodeAlts: ""
+	}
+	ccode = "ccode",
+	ccodeAlts = "ccodeAlts",
+	printCode = function (dest, str) {
 		console.log(str);
-		ccode += str + "\n";
+		results[dest] += str + "\n";
+	},
+	swapInArray = function (arr, fromIdx, toIdx) {
+		var tmp = arr[toIdx];
+
+		arr[toIdx] = arr[fromIdx];
+		arr[fromIdx] = tmp;
+
+		return arr;
 	};
 
 
+for (var key = 0; key < 12; key++) {
 
-for (var k in scales) { 
-	var n = scales[k];
-
-	printCode('// Scale ' + k);
-
-	for (var r = 0; r < roots.length; r++) {
-		var res = [];
-
-	    for (j in n) {
-	        res[j] = n[j] + roots[r];
-	    }
-	    console.log(res);
-
-	    printCode("{" + res.join(",")  + "},");
+	for (var keyIdx = 0; keyIdx < roots.length; keyIdx++) {
+		roots[keyIdx] += 1;
 	}
 
+	printCode(ccode, '// Key ' + roots[1]);
+	printCode(ccodeAlts, '// Key ' + roots[1]);
+
+
+	for (var k in scales) { 
+		var notes = scales[k];
+
+		printCode(ccode, '// Scale ' + k);
+		printCode(ccodeAlts, '// Alt Scale ' + k);
+
+		for (var r = 0; r < roots.length; r++) {
+			var res = [];
+
+		    for (j in notes) {
+		        res[j] = notes[j] + roots[r];
+		    }
+		    for (var ext = res.length; ext < TOTAL_ARRAY_LEN; ext++) {
+		    	res[ext] = notes[(ext - notes.length) + 1] + (roots[r] + 12);
+		    }
+
+		    printCode(ccode, "{" + res.join(", ")  + "},");
+
+		    // make pentatonic version (hexatonic, really)
+		    res = swapInArray(res, 3, 6); // take out 4th, replace with 7th
+		    printCode(ccodeAlts, "{" + res.join(", ")  + "},");
+		}
+
+	}
 }
 
-console.log(ccode);
+console.log(results.ccode);
+console.log(results.ccodeAlts);
